b871099348deb45978690e8855be05d620315e4f
Cristian Jimenez <cjimenez2.dam@institutcampalans.net>
2025-05-19 19:21:57 +0200

    ultims canvis

--- ExercciA6/obj/Debug/net9.0/ExercciA6.AssemblyInfo.cs
+++ ExercciA6/obj/Debug/net9.0/ExercciA6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercciA6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2247b2415c77309c310a8e2d9122ee29ebdbd84a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c21d83bc7ca6f0adf598715772dd561741f4a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercciA6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercciA6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
